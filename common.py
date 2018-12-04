5c4b11c02367f183cb4e6d9172ad12ee64fee68b
Alex Shan <36291011+shan-mx@users.noreply.github.com>
2018-12-04 21:57:03 +0800

    Add files via upload

--- common.py
+++ common.py
@@ -36,9 +36,13 @@ def get_movie_ftp(target_name):
         if not soup.find_all("a", class_="ulink"):
             break
         for each_title in soup.find_all("a", class_="ulink"):
-            if re.match("(.*)《(.*)》(.*)", each_title.text):
-                movie_name = re.match("(.*)《(.*)》(.*)", each_title.text).group(2)
+            #print(each_title.text)
+            match = re.match("(.*)《(.*)(/)?(.*)》(.*)", each_title.text)
+            if match:
+                movie_name = match.group(2)
+
                 if re.match(target_name, movie_name):
+                    print(movie_name)
                     films.append({"name": movie_name, "href": each_title["href"]})
     download_url = None
     for each in films:
@@ -54,7 +58,7 @@ def get_movie_ftp(target_name):
                 break
     if download_url == None:
         return None
-
+    print(download_url)
     response = urllib.request.urlopen(download_url)
     html = response.read().decode('gbk')
     soup = BeautifulSoup(html, "html.parser")
@@ -62,21 +66,40 @@ def get_movie_ftp(target_name):
     ftp_urls = []
     for each in soup.find_all("td",bgcolor="#fdfddf"):
         url = each.text.replace("\n","")
-        if url[:4] == "ftp:":
+        if url[:4] == "ftp:" or url[:8] == "thunder:" or url[:7] == "magnet:":
             ftp_urls.append(url)
     return ftp_urls
 
 if __name__ == "__main__":
     print("输入电影关键字:")
-    movie_name = input(":->")
-    print("查询中，请稍候...")
-    urls = get_movie_ftp(movie_name)
-    if urls == None:
-        print("暂时无法找到" + '"' + sys.argv[2] + '"的资源！ ')
-        time.sleep(4)
-        exit(0)
+    query_name = input(":->")
+    urls = get_movie_ftp(query_name)
+    if urls == None or len(urls) == 0:
+        print("暂时无法找到" + '"' + query_name + '"的资源！ ')
+        input()
 
-    print("复制以下链接至迅雷（或其他bt下载工具）:")
+    print("查询结果链接:")
+    print(urls)
     for each in urls:
         tmp = each.split("/")
         print(each)
+
+    thunder_path = "C:\Program Files (x86)\Thunder Network\Thunder\Program\Thunder.exe"
+    with open("thunder_path", "r") as f:
+        thunder_path = f.read()
+    if not os.path.exists(thunder_path):
+        print("无法找到迅雷路径！")
+        print("如果电脑内安装了迅雷（或其他bt/ed2k下载工具），请用记事本或其他文本编辑工具"
+              "打开thunder_path文件，删除其中内容后把下载工具的路径粘贴到此中。")
+        input()
+    else:
+        print("找到了本机迅雷位置，正在自动下载：")
+        for each in urls:
+            tmp = each.split("/")
+            print("正在为您下载：" + tmp[len(tmp) - 1])
+            os.system('"C:\Program Files (x86)\Thunder Network\Thunder\Program\Thunder.exe" ' + each)
+            time.sleep(5)
+            pyautogui.press("enter")
+            time.sleep(5)
+            pyautogui.press("enter")
+            input()
